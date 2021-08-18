baad92f3d13fd27f7d2f446122d48f27a812ca14
Ali Syahidin <ali.syahidin@sg-dsa.com>
2021-08-18 18:51:26 +0700

    add sample of shared layout animation

--- pages/shared-layout-animation.js
+++ pages/shared-layout-animation.js
@@ -10,7 +10,7 @@ const ListItem = () => {
     <AnimatePresence>
       {isOpen && <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="h-10 bg-gray-300 rounded" />}
     </AnimatePresence >
-  </motion.li >
+  </motion.li>
 }
 
 const List = () => {
@@ -43,8 +43,8 @@ const ListItem = () => {
 `.trim()
 
   return (
-    <div className="h-5/6 w-4/5">
-      <h1 className="text-white text-3xl mb-4">Shared layout list</h1>
+    <div className="flex flex-col justify-center h-5/6 w-4/5">
+      <h1 className="text-white text-3xl mb-4">Shared layout child component's state</h1>
       <div className="flex shadow rounded-xl bg-white bg-opacity-20 blur">
         <div className="flex items-center justify-center w-full overflow-hidden relative">
           {layout
@@ -84,6 +84,80 @@ const ListItem = () => {
   )
 }
 
+const MenuItem = ({ index, title, activeMenu, setActiveMenu }) => (
+  <li onClick={() => setActiveMenu(index)} className={`w-${index + 2}/5 text-center cursor-pointer`}>
+    <p className="my-2">{title}</p>
+    {activeMenu === index && <motion.div layoutId="under-line" className="h-2 bg-red-400 w-full" />}
+  </li>
+)
+
+const NavigationMenu = () => {
+  const [layout, setLayout] = useState(true);
+  const [activeMenu, setActiveMenu] = useState(0)
+
+  const getCode = (layout) => `
+${!layout ? '// ' : ''}<AnimateSharedLayout>
+  <motion.ul layout>
+    {menus.map(index => (
+      <MenuItem key={index} />
+    ))}
+  </motion.ul>
+${!layout ? '// ' : ''}</AnimateSharedLayout>
+
+const MenuItem = () => (
+  <li onClick={() => setActiveMenu(index)}>
+    <p>{title}</p>
+    {activeMenu === index && (
+      <motion.div layoutId="under-line" />
+    )}
+  </li>
+)
+`.trim()
+
+  const menus = ['Home', 'About', 'Contact']
+
+  return (
+    <div className="flex flex-col justify-center h-5/6 w-4/5">
+      <h1 className="text-white text-3xl mb-4">Shared layout component with layoutId</h1>
+      <div className="flex shadow rounded-xl bg-white bg-opacity-20 blur">
+        <div className="flex items-center justify-center w-full overflow-hidden relative">
+          {layout
+            ? (
+              <AnimateSharedLayout>
+                <ul className="flex bg-white w-5/6">
+                  {menus.map((title, index) => (
+                    <MenuItem key={index} index={index} title={title} activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+                  ))}
+                </ul>
+              </AnimateSharedLayout>
+            ) : (
+              <ul className="flex bg-white w-5/6">
+                {menus.map((title, index) => (
+                  <MenuItem key={index} index={index} title={title} activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+                ))}
+              </ul>
+            )}
+        </div>
+        <div className="flex bg-gray-900 rounded-r-xl text-white">
+          <div className="flex p-4 w-96">
+            <Highlight language="jsx">
+              {getCode(layout)}
+            </Highlight>
+          </div>
+          <div className="p-3 w-52">
+            <div>
+              <div className="flex items-center justify-between">
+                <strong>Shared Layout</strong>
+                <input type="checkbox" onChange={e => setLayout(e.target.checked)} checked={layout} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function SharedLayoutAnimation() {
   return (<>
     <Head>
@@ -94,7 +168,7 @@ function SharedLayoutAnimation() {
       <List />
     </div>
     <div className="flex flex-col justify-center items-center h-screen">
-      <List />
+      <NavigationMenu />
     </div>
   </>)
 }
