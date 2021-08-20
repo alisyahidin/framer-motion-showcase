332591a1ff8ee5908fff3d8db5aea52b0b1cd0fe
Ali Syahidin <ali.syahidin@sg-dsa.com>
2021-08-20 10:38:55 +0700

    add sample of basic animation

--- pages/basic-animation.js
+++ pages/basic-animation.js
@@ -107,12 +107,8 @@ function AnimationProps() {
 }
 
 function AnimationKeyframes() {
-
-  const [initial, setinitial] = useState(true);
-
   const getCode = () => `
 <motion.div
-  className="w-24 h-24 bg-white"
   animate={{
     x: [0, 20, 30, 60, 0],
     y: [0, 35, 70, 50, 0],
@@ -156,6 +152,82 @@ function AnimationKeyframes() {
   )
 }
 
+function AnimationVariant() {
+  const [isMounted, setIsMounted] = useState(false)
+  const getCode = (isMounted) => `
+const wrapper = {
+  hide: { opacity: 0 },
+  show: { opacity: 1 }
+}
+
+const item = {
+  hide: { opacity: 0, y: 15 },
+  show: { opacity: 1, y: 0 }
+}
+
+<motion.ul variants={wrapper} animate="${isMounted ? 'show' : 'hide'}">
+  {items.map(index => (
+    <motion.li key={index} variants={item} />
+  ))}
+</motion.ul>`.trim()
+
+  const wrapper = {
+    hide: {
+      opacity: 0,
+      transition: {
+        delay: 0.6,
+        staggerDirection: -1,
+        staggerChildren: 0.1
+      }
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.15
+      }
+    },
+  }
+
+  const item = {
+    hide: {
+      opacity: 0,
+      y: 15
+    },
+    show: {
+      opacity: 1,
+      y: 0
+    },
+  }
+
+  return (
+    <div className="w-4/5">
+      <h1 className="text-white text-3xl mb-4">Animation Variant</h1>
+      <div className="flex shadow rounded-xl bg-white bg-opacity-20 blur">
+        <div className="flex items-center justify-center w-full overflow-hidden relative">
+          <button onClick={() => setIsMounted(!isMounted)} className="absolute bg-black bg-opacity-20 text-white py-1 px-2 bottom-3 right-3 rounded">
+            {isMounted ? 'Hide' : 'Show'}
+          </button>
+          <motion.ul variants={wrapper} animate={isMounted ? 'show' : 'hide'} className="flex flex-col gap-3 p-4 bg-gray-100 w-64 rounded-lg">
+            <motion.li variants={item} className={`w-full h-12 rounded-lg bg-purple-400`} />
+            <motion.li variants={item} className={`w-full h-12 rounded-lg bg-red-400`} />
+            <motion.li variants={item} className={`w-full h-12 rounded-lg bg-yellow-400`} />
+            <motion.li variants={item} className={`w-full h-12 rounded-lg bg-green-400`} />
+            <motion.li variants={item} className={`w-full h-12 rounded-lg bg-blue-400`} />
+          </motion.ul>
+        </div>
+        <div className="flex bg-gray-900 rounded-r-xl text-white">
+          <div className="flex p-3">
+            <Highlight language="jsx">
+              {getCode(isMounted)}
+            </Highlight>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function BasicAnimation() {
   return (<>
     <Head>
@@ -167,6 +239,9 @@ export default function BasicAnimation() {
     </div>
     <div className="flex flex-col justify-center items-center h-screen">
       <AnimationKeyframes />
+    </div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <AnimationVariant />
     </div>
   </>)
 }
